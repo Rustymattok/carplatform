f5268b8fd9a06d1f2e425aeaa4f3b7f9890990a9
Rustymattok <mrArxi@gmail.com>
2020-02-26 08:33:41 +0300

    add

--- ParsingAutoRu/src/main/java/Parsing.java
+++ ParsingAutoRu/src/main/java/Parsing.java
@@ -3,9 +3,9 @@ import parsing.ParsingMetaCar;
 
 public class Parsing {
     public static void main(String[] args) {
-        ParsingDataBase parsingDataBase = new ParsingDataBase();
-        parsingDataBase.createDataBaseModels();
-        ParsingMetaCar parsingMetaCar = new ParsingMetaCar();
-        parsingMetaCar.saveToFile(parsingMetaCar.jsonItemsList(),"./ParsingAutoRu/src/main/resources/lastitems.json");
+//        ParsingDataBase parsingDataBase = new ParsingDataBase();
+//        parsingDataBase.createDataBaseModels();
+//        ParsingMetaCar parsingMetaCar = new ParsingMetaCar();
+//        parsingMetaCar.saveToFile(parsingMetaCar.jsonItemsList(),"./ParsingAutoRu/src/main/resources/lastitems.json");
     }
 }
