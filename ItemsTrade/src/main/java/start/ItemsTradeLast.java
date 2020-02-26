85fa7a593a8e68dd88ee0d90066240e75e86779c
Rustymattok <mrArxi@gmail.com>
2020-02-26 08:33:14 +0300

    add

--- ItemsTrade/src/main/java/start/ItemsTradeLast.java
+++ ItemsTrade/src/main/java/start/ItemsTradeLast.java
@@ -4,8 +4,8 @@ import presistens.DAOParsingItems;
  * Script for model launch.
  */
 public class ItemsTradeLast {
-    public static void main(String[] args) {
-        DAOParsingItems daoParsingItems = DAOParsingItems.getINSTANCE();
-        daoParsingItems.addItemsLast(daoParsingItems.getItem(daoParsingItems.getItemConvert("./ParsingAutoRu/src/main/resources/lastitems.json")));
-    }
+//    public static void main(String[] args) {
+//        DAOParsingItems daoParsingItems = DAOParsingItems.getINSTANCE();
+//        daoParsingItems.addItemsLast(daoParsingItems.getItem(daoParsingItems.getItemConvert("./ParsingAutoRu/src/main/resources/lastitems.json")));
+//    }
 }
